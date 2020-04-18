3aafedb6decc26b77e31938b79327fbb796c1b97
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-04-18 22:06:06 +0000

    added event handlers in form component, props from app are being bpassed successfully to grade-form, DOM updates after db updates properly... no form validation setup right now

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -9,6 +9,7 @@ class App extends React.Component {
     this.state = {
       grades: []
     };
+    this.postGrade = this.postGrade.bind(this);
   }
 
   componentDidMount() {
@@ -20,12 +21,31 @@ class App extends React.Component {
       .catch(err => console.error(err));
   }
 
+  postGrade(newGrade) {
+    const fetchConfig = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newGrade)
+    };
+
+    fetch('/api/grades', fetchConfig)
+      .then(res => res.json())
+      .then(newGrade => {
+        const gradesCopy = [...this.state.grades];
+        gradesCopy.push(newGrade);
+        this.setState({ grades: gradesCopy });
+      })
+      .catch(err => console.error(err));
+  }
+
   getAverageGrade() {
     const { grades } = this.state;
     let average = 0;
 
     for (let i = 0; i < grades.length; i++) {
-      average += grades[i].grade;
+      average += parseFloat(grades[i].grade);
     }
     average /= grades.length;
 
@@ -41,7 +61,7 @@ class App extends React.Component {
         />
         <div className="row">
           <GradeTable grades={this.state.grades}/>
-          <GradeForm />
+          <GradeForm onSubmit={this.postGrade}/>
         </div>
       </div>
     );
--- client/components/grade-form.jsx
+++ client/components/grade-form.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
 
 class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      course: '',
+      grade: 0
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCourseChange = this.handleCourseChange.bind(this);
+    this.handleGradeChange = this.handleGradeChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleCourseChange(event) {
+    this.setState({ course: event.target.value });
+  }
+
+  handleGradeChange(event) {
+    this.setState({ grade: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { name, course, grade } = this.state;
+
+    const newGrade = {
+      name: name,
+      course: course,
+      grade: grade
+    };
+
+    this.props.onSubmit(newGrade);
+    this.setState({
+      name: '',
+      course: '',
+      grade: 0
+    });
+  }
+
   render() {
     const labelClasses = 'col-form-label col-2 card align-items-center bg-light';
     const inputClasses = 'form-control col-10';
     const formRowClasses = 'form-group row';
     return (
-      <form className="col-12 col-lg-3 px-0">
+      <form
+        className="col-12 col-lg-3 px-0"
+        onSubmit={this.handleSubmit}
+      >
         <h4 className="form-title" id="formTitle">Add Grade</h4>
         <div className={formRowClasses}>
           <label htmlFor="name" className={labelClasses}>
@@ -18,6 +65,7 @@ class GradeForm extends React.Component {
             id="name"
             name="name"
             placeholder="Name"
+            onChange={this.handleNameChange}
           />
         </div>
         <div className={formRowClasses}>
@@ -30,6 +78,7 @@ class GradeForm extends React.Component {
             id="course"
             name="course"
             placeholder="Course"
+            onChange={this.handleCourseChange}
           />
         </div>
         <div className={formRowClasses}>
@@ -42,11 +91,21 @@ class GradeForm extends React.Component {
             id="grade"
             name="grade"
             placeholder="Grade"
+            onChange={this.handleGradeChange}
           />
         </div>
         <div className={formRowClasses}>
-          <button className="btn btn-success mx-2" type="submit" id="formSubmit">Add</button>
-          <button className="btn btn-secondary" type="reset">Cancel</button>
+          <button
+            className="btn btn-success mx-2"
+            type="submit"
+            id="formSubmit">
+              Add
+          </button>
+          <button
+            className="btn btn-secondary"
+            type="reset">
+              Cancel
+          </button>
         </div>
       </form>
     );
